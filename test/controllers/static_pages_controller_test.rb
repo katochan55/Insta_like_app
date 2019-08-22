8b6c450515924985acde6f25a8be6a06abfa06fb
EC2 Default User <ec2-user@ip-172-31-43-108.ap-northeast-1.compute.internal>
2019-08-22 12:59:24 +0000

    Finish adding static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Instagram clone"
+  end
+  
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "トップ | #{@base_title}"
   end
 
   test "should get terms" do
     get static_pages_terms_url
     assert_response :success
+    assert_select "title", "利用規約 | #{@base_title}"
   end
 
 end
