83538a78a56dcf74bbbe58ebe4e82d1302a0329b
EC2 Default User <ec2-user@ip-172-31-43-108.ap-northeast-1.compute.internal>
2019-08-25 00:15:56 +0000

    Add log_in_as method for test login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
+class ActiveSupport::TestCase # 単体テスト用メソッド
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
@@ -10,6 +10,20 @@ class ActiveSupport::TestCase
   # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
+  end
+  
+  # テストユーザーとしてログインする（単体テスト用）
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest # 統合テスト用メソッド
+
+  # テストユーザーとしてログインする(統合テスト用)
+  def log_in_as(user, password: 'password')
+    post login_path, params: { session: { email: user.email,
+                                          password: password } }
   end
 
 end
