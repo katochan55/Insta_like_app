0b24c09368f8a77fcb1b38a284228b2f2db41076
EC2 Default User <ec2-user@ip-172-31-43-108.ap-northeast-1.compute.internal>
2019-08-27 14:29:01 +0000

    Add Microposts controller and place @microposts in show page

---
+++ test/integration/users_profile_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper # full_titleヘルパーを使用するため
+
+  def setup
+    @user = users(:taro)
+  end
+
+  # プロフィール画面の表示
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.full_name, @user.user_name)
+    assert_select 'h1', text: @user.user_name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+end
