ab7844fabf64a81e4244030e98232bf6315558a8
EC2 Default User <ec2-user@ip-172-31-43-108.ap-northeast-1.compute.internal>
2019-08-31 04:04:48 +0000

    Add validation to format of picture

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,15 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
+  validate  :picture_size
+
+
+  private
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "5MBより大きい画像はアップロードできません。")
+      end
+    end
+    
 end
