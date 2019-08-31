136af6764ab45579dfcc6e5debcb6d2104d16c00
EC2 Default User <ec2-user@ip-172-31-43-108.ap-northeast-1.compute.internal>
2019-08-31 13:37:08 +0000

    Add follow and unfollow information, button

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   root 'static_pages#home' # ランディングページ
   get  '/use_of_terms', to: 'static_pages#terms' # 利用規約ページ
   
-  # Usersコントローラのルーティング
+  # Usersコントローラのルーティング（followingアクションとfollowersアクションも追加）
   get '/signup', to: 'users#new' # ユーザー登録ページ
   post '/signup',  to: 'users#create' # ユーザー登録時のフォーム送信
-  resources :users
-  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # Sessionsコントローラのルーティング
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
@@ -19,5 +22,8 @@ Rails.application.routes.draw do
 
   # Micropostsコントローラのルーティング  
   resources :microposts, only: [:show, :new, :create, :destroy]
+  
+  # Relationshipリソースのルーティング
+  resources :relationships, only: [:create, :destroy]
   
 end
